93ddef1283bf106963c9820e5ced6a837f9c384f
CarsonBain <carsonbain@gmail.com>
2021-04-11 17:45:44 -0400

    initial commit

---
+++ index-pipedream.js
@@ -0,0 +1,41 @@
+// This version includes returning availableSlots with `this` and awaits getDataForDays
+
+const axios = require('axios');
+
+const numberOfDays = [0, 1, 2, 3, 4, 5, 6, 7];
+const availableSlots = [];
+
+async function getDataForDays() {
+  for (let day in numberOfDays) {
+    await getData(day);
+  }
+  console.log(availableSlots);
+}
+
+async function getData(day) {
+  let today = new Date();
+  let dateToLookFor = today.setDate(today.getDate() + Number(day));
+
+  const url = `https://camh.vertoengage.com/engage/api/api/cac-open-clinic/v1/slots/availability?day=${new Date(
+    dateToLookFor
+  ).toISOString()}&location_id=CMH&slot_type=CONGREGATE&key=e9ae7abf-1c88-479b-9c1a-2e81a21da988`;
+
+  await axios(url)
+    .then((response) => {
+      const data = response.data;
+      if (data.slots_left !== 0) {
+        availableSlots.push(new Date(dateToLookFor).toDateString());
+      }
+    })
+    .catch(console.error);
+}
+
+await getDataForDays().then(() => {
+  if (!availableSlots.length) {
+    // Don't run anymore workflow steps
+    $end();
+  } else {
+    // Format the available dates into lists for slack message
+    return (this.availableSlots = availableSlots.join('\n• '));
+  }
+});
